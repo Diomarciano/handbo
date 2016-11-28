c77e00d0c589c9028040b827c7844fc7af3de7b8
ebennaga <freddy.eben@gmail.com>
2016-11-29 00:12:44 +0100

    update class

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,2 @@
 class UserMailer < ApplicationMailer
- 
- def welcome_email(user)
-    @user = user
-    @url  = 'http://handbo.de/login'
-    mail(to: @user.email, subject: 'Welcome to Our Awesome Site')
- end
-
 end
